442af722d5f83be9e15cbd86d22a17b8b7e06d5d
AlimulK <87857540+AlimulK@users.noreply.github.com>
2022-03-02 09:56:24 +0000

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ import lejos.utility.Delay;
 public class Main {
 	
 	// change the version number - 1.0 will be the final version
-	final static Float version = 1.0f;
+	final static String VERSION = "1.0";
 	
 	// constants
 	final static int WHEEL_DIAMTER = 56;
@@ -21,7 +21,7 @@ public class Main {
 		
 		// welcome message
 		LCD.drawString("Ali, Amrit, Ash, Kamil", 1, 1);
-		LCD.drawString("Version: " + version.toString(), 1, 2);
+		LCD.drawString("Version: " + VERSION, 1, 2);
 		LCD.drawString("Please press a button", 1, 1);
 		
 		// infinite delay until enter is pressed
@@ -40,10 +40,43 @@ public class Main {
 		NXTSoundSensor ss = new NXTSoundSensor(SensorPort.S4);
 		
 		// sample providers
-		SampleProvider dm = us.getDistanceMode();
-		SampleProvider rm = cs1.getRedMode();
-		SampleProvider cm = cs2.getRGBMode();
-		SampleProvider sm = ss.getDBAMode();
+		SampleProvider dm = us.getDistanceMode(); // object detection
+		SampleProvider rm = cs1.getRedMode(); // line follow sensor
+		SampleProvider cm = cs2.getRGBMode(); // colour detection sensor
+		SampleProvider sm = ss.getDBAMode(); // clap sensor
+		
+		// samples array
+		float[] levels = new float[4];
+		
+		// calibration
+		float maxL = 0.0f;
+		float minL = 1.0f;
+		
+		while(!Button.DOWN.isDown()) {
+			rm.fetchSample(levels, 1);
+			
+			if(levels[1] > maxL) {
+				maxL = levels[1];
+			}
+			
+			if (levels[1] < minL) {
+				minL = levels[1];
+			}
+			
+			LCD.drawString("Max =" + Float.toString(maxL), 2, 2);
+			LCD.drawString("Press 'Down' to complete calibration",  2, 4);
+			LCD.drawString("Min =" + Float.toString(minL), 2, 6);
+			
+			Delay.msDelay(500);
+			
+			LCD.clear();
+		}
+		
+		// exact average - PREFERABLE USE APPROX
+		float lightAv = ((maxL + minL)/2);
+		// approx average
+		float lightUp = lightAv + (lightAv * 0.1f);
+		float lightLo = lightAv - (lightAv * 0.1f);
 		
 	}
 

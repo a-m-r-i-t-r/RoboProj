7971401b306a805c45953da031cef4eced5b767c
KamilAscendant <98907106+KamilAscendant@users.noreply.github.com>
2022-03-16 02:35:11 +0000

    Add files via upload
    when I figure out what each color's id constant is this will be finished but the api doesnt bloody say so gotta test it in-lab

---
+++ src/SpeedChanger.java
@@ -0,0 +1,52 @@
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.subsumption.Behavior;
+
+public class SpeedChanger implements Behavior{
+	
+	private BaseRegulatedMotor mL;
+	private BaseRegulatedMotor mR;
+	private boolean suppress = false;
+	private EV3ColorSensor light;
+	private int SPEED = 100;
+	
+	SpeedChanger(BaseRegulatedMotor mL, BaseRegulatedMotor mR, EV3ColorSensor light, float lightUp, float lightLo) {
+		this.mL = mL;
+		this.mR = mR;
+		this.light = light;
+		
+		mL.setSpeed(SPEED);
+		mR.setSpeed(SPEED);
+	}
+
+	@Override
+	public boolean takeControl() {
+		return true;
+	}
+
+	@Override
+	public void action() {
+		int ll = light.getColorID();
+		
+		while(!suppress) {	
+			if (ll == 1) {
+
+				SPEED = SPEED * 2;
+				mL.setSpeed(SPEED);
+				mR.setSpeed(SPEED);
+			}
+			
+			else if (ll == 2) {
+				SPEED = SPEED / 2;
+				mL.setSpeed(SPEED);
+				mR.setSpeed(SPEED);
+			}
+		}
+	}
+
+	@Override
+	public void suppress() {
+		suppress = true;
+	}
+}
+
